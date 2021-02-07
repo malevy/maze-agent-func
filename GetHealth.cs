fbbb051f6a03294d12de4ae3be4809d3c745b1f2
Michael Levy <malevy@outlook.com>
2021-02-07 13:30:34 -0500

    add health endpoint

---
+++ GetHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace maze_agent_func
+{
+    public static class GetHealth
+    {
+        public class GetHealthResponse
+        {
+            [JsonProperty("status")]
+            public string Status;
+        }
+        
+        [FunctionName("GetHealth")]
+        public static async Task<IActionResult> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = ".well-known/health")]
+            HttpRequest req, 
+            ILogger log)
+        {
+            log.LogInformation("getHealth-entry");
+
+            var response = new GetHealthResponse
+            {
+                Status = "up"
+            };
+            
+            log.LogInformation("getHealth-exit");
+
+            return new OkObjectResult(response);
+
+        }
+    }
+}
